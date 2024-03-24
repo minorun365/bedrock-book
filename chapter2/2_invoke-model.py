cbbe2d6fccabe33de7602e048540ccb4abb8a5f7
みのるん <74597894+minorun365@users.noreply.github.com>
2024-03-24 17:05:48 +0900

    Create 2_invoke-model.py

---
+++ chapter2/2_invoke-model.py
@@ -0,0 +1,35 @@
+# Python外部ライブラリをインポート
+import json
+import boto3
+
+# Bedrockクライアントを作成
+bedrock = boto3.client("bedrock-runtime")
+
+# リクエストボディを定義
+body = json.dumps(
+    {
+        "anthropic_version": "bedrock-2023-05-31",
+        "max_tokens": 1000,
+        "messages": [
+            {
+                "role": "user",
+                "content": "日本の総理大臣を新しい順に3名、リストアップしてください",
+            }
+        ],
+    }
+)
+
+# モデルを定義（Claude 3 Sonnet）
+modelId = "anthropic.claude-3-sonnet-20240229-v1:0"
+
+# HTTPヘッダーを定義
+accept = "application/json"
+contentType = "application/json"
+
+# レスポンスを定義
+response = bedrock.invoke_model(body=body, modelId=modelId, accept=accept, contentType=contentType)
+response_body = json.loads(response.get("body").read())
+answer = response_body["content"][0]["text"]
+
+# 生成されたテキストをコンソールに表示
+print(answer)
