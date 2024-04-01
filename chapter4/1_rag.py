550394f0ba073e97f39becd68b6a319993bdfae0
みのるん <74597894+minorun365@users.noreply.github.com>
2024-04-01 21:27:07 +0900

    Update 1_rag.py

--- chapter4/1_rag.py
+++ chapter4/1_rag.py
@@ -19,7 +19,8 @@ prompt = ChatPromptTemplate.from_template(
 
 # LLMを指定
 model = BedrockChat(
-    model_id="anthropic.claude-3-haiku-20240307-v1:0", model_kwargs={"max_tokens": 1000}
+    model_id="anthropic.claude-3-sonnet-20240229-v1:0", 
+    model_kwargs={"max_tokens": 1000}
 )
 
 # チェーンを定義（検索 → プロンプト作成 → LLM呼び出し → 結果を取得）
