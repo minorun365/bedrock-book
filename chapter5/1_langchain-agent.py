a13139f133bdab0dffffaa6ff5b4a8888f999b20
kumada15 <kan.kumada@relic.co.jp>
2024-04-03 03:55:35 +0900

    エラー対応及び改善

--- chapter5/1_langchain-agent.py
+++ chapter5/1_langchain-agent.py
@@ -1,18 +1,17 @@
-import asyncio
 import nest_asyncio
 import streamlit as st
+from bs4 import BeautifulSoup
 from langchain import hub
 from langchain.agents import AgentExecutor, Tool, create_xml_agent
-from langchain.agents import create_xml_agent, AgentExecutor, Tool
-from langchain.prompts.prompt import PromptTemplate
 from langchain_community.chat_models import BedrockChat
-from langchain_community.callbacks import StreamlitCallbackHandler
-from langchain_community.tools import DuckDuckGoSearchRun
 from langchain_community.document_loaders import WebBaseLoader
-from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
+from langchain_community.tools import DuckDuckGoSearchRun
+from langchain_core.messages import HumanMessage, SystemMessage
+
+nest_asyncio.apply()
 
 # Web ページの内容を読み込む関数
-async def web_page_reader(url: str) -> str:
+def web_page_reader(url: str) -> str:
     loader = WebBaseLoader(url)
     content = loader.load()[0].page_content
     return content
@@ -22,14 +21,14 @@ search = DuckDuckGoSearchRun()
 tools = [
     Tool(
         name="duckduckgo-search",
-        func=search.arun,
-        description="このツールはユーザから検索キーワードを受け取り、Web上の最新情報を検索します。",
+        func=search.run,
+        description="このツールはユーザーから検索キーワードを受け取り、Web上の最新情報を検索します。",
     ),
     Tool(
         name="WebBaseLoader",
         func=web_page_reader,
-        description="このツールはユーザからURLを渡された場合に内容をテキストを返却します。URLの文字列のみを受け付けます。",
-    )
+        description="このツールはユーザーからURLを渡された場合に内容をテキストを返却します。URLの文字列のみを受け付けます。",
+    ),
 ]
 
 # チャットモデルの設定
@@ -54,23 +53,15 @@ agent_executor = AgentExecutor(
 
 # Streamlit アプリケーションの設定
 st.title("Bedrock Agent チャット")
-messages = [
-    SystemMessage(content="あなたは質問に対して必ず日本語で回答します。")
-]
+messages = [SystemMessage(content="あなたは質問に対して必ず日本語で回答します。")]
 
 # ユーザー入力の処理
-if prompt := st.chat_input("何でも聞いてください。"):
+prompt = st.chat_input("何でも聞いてください。")
+if prompt:
     messages.append(HumanMessage(content=prompt))
-
     with st.chat_message("user"):
         st.markdown(prompt)
-
     with st.chat_message("assistant"):
-        st_callback = StreamlitCallbackHandler(st.container())
-
-        # 非同期処理でエージェントを呼び出す
-        result = asyncio.run(agent_executor.ainvoke(
-            {"input": prompt},
-        ))
-
+        # エージェントを呼び出す
+        result = agent_executor.invoke({"input": prompt})
         st.write(result["output"])
