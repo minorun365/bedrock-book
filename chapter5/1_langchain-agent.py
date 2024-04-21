2a6b07115d5c97b43c59fa4220dc12617f7d84af
kumada15 <kan.kumada@relic.co.jp>
2024-04-21 18:53:55 +0900

    update

--- chapter5/1_langchain-agent.py
+++ chapter5/1_langchain-agent.py
@@ -3,7 +3,7 @@ import streamlit as st
 from bs4 import BeautifulSoup
 from langchain import hub
 from langchain.agents import AgentExecutor, Tool, create_xml_agent
-from langchain_community.chat_models import BedrockChat
+from langchain_aws import ChatBedrock
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_community.tools import DuckDuckGoSearchRun
 from langchain_core.messages import HumanMessage, SystemMessage
@@ -32,7 +32,7 @@ tools = [
 ]
 
 # チャットモデルの設定
-chat = BedrockChat(
+chat = ChatBedrock(
     model_id="anthropic.claude-3-sonnet-20240229-v1:0",
     model_kwargs={"max_tokens": 1500},
 )
--- chapter5/2_langchain-react-agent.py
+++ chapter5/2_langchain-react-agent.py
@@ -1,15 +1,11 @@
-import asyncio
-
 import nest_asyncio
 import streamlit as st
+from bs4 import BeautifulSoup
 from langchain import hub
 from langchain.agents import AgentExecutor, Tool, create_react_agent
-from langchain.prompts.prompt import PromptTemplate
-from langchain_community.callbacks import StreamlitCallbackHandler
-from langchain_community.chat_message_histories import StreamlitChatMessageHistory
-from langchain_community.chat_models import BedrockChat
+from langchain_aws import ChatBedrock
 from langchain_community.tools import DuckDuckGoSearchRun
-from langchain_core.messages import AIMessage, HumanMessage
+from langchain_core.messages import HumanMessage, SystemMessage
 
 nest_asyncio.apply()
 
@@ -18,36 +14,33 @@ tools = [
     Tool(
         name="duckduckgo-search",
         func=search.run,
-        description="このツールはWeb上の最新情報を検索します。ユーザから検索キーワードを受け取ります。最新情報が必要ない場合はこのツールは使用しません。",
+        description="このツールはユーザーから検索キーワードを受け取り、Web上の最新情報を検索します。",
     )
 ]
 
 # チャットモデルの設定
-chat = BedrockChat(
+chat = ChatBedrock(
     model_id="anthropic.claude-3-sonnet-20240229-v1:0",
-    model_kwargs={"max_tokens": 1000},
-    streaming=True,
+    model_kwargs={"max_tokens": 1500},
 )
 
-prompt = hub.pull("hwchase17/react")
-
 # エージェントの設定
-agent = create_react_agent(chat, tools, prompt)
+agent = create_react_agent(chat, tools, prompt=hub.pull("hwchase17/react"))
 agent_executor = AgentExecutor(
     agent=agent, tools=tools, verbose=True, handle_parsing_errors=True
 )
 
 # Streamlit アプリケーションの設定
 st.title("Bedrock ReAct Agent チャット")
+messages = [SystemMessage(content="あなたは質問に対して必ず日本語で回答します。")]
 
-async def run_agent(prompt):
-    with st.chat_message("assistant"):
-        st_callback = StreamlitCallbackHandler(st.container())
-        result = await agent_executor.ainvoke({"input": prompt})
-        st.write(result["output"])
-
-if prompt := st.chat_input("何でも聞いてください。"):
+# ユーザー入力の処理
+prompt = st.chat_input("何でも聞いてください。")
+if prompt:
+    messages.append(HumanMessage(content=prompt))
     with st.chat_message("user"):
         st.markdown(prompt)
-
-    asyncio.run(run_agent(prompt))
+    with st.chat_message("assistant"):
+        # エージェントを呼び出す
+        result = agent_executor.invoke({"input": prompt})
+        st.write(result["output"])
